6ae6317367dbea0eefd41df1865acb21527dfe3a
OneMind-Dev <anhkvse182347@fpt.edu.vn>
2024-12-07 12:06:30 +0700

    login, user profile

--- ChatBot/WPF/MainWindow.xaml.cs
+++ ChatBot/WPF/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using BOs;
+using Services.Implement;
+using Services.Interface;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +19,44 @@ namespace WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        public IUserService UserService;
         public MainWindow()
         {
             InitializeComponent();
+            UserService = new UserService();
+        }
+
+        
+        
+
+        private void Button_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            // Xóa nội dung của các ô nhập
+            txtUser.Clear();
+            txtPass.Clear();
+        }
+
+        private void Button_Login_Click(object sender, RoutedEventArgs e)
+        {
+
+            string username = txtUser.Text;
+            string password = txtPass.Password;
+
+            // Gọi phương thức LoginUser từ IUserService
+            User loggedInUser = UserService.LoginUser(username, password);
+
+            if (loggedInUser != null)
+            {
+                MessageBox.Show($"Login successful! Welcome, {loggedInUser.Username}", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                // Di chuyển đến cửa sổ chính hoặc màn hình tiếp theo ở đây
+                UserProfileWindow userProfileWindow = new UserProfileWindow(loggedInUser);
+                userProfileWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
---
+++ ChatBot/WPF/UserProfileWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using BOs;
+using Services.Implement;
+using Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for UserProfileWindow.xaml
+    /// </summary>
+    public partial class UserProfileWindow : Window
+    {
+        public IUserService UserService;
+        private User LoggedUser;
+        public UserProfileWindow(User user)
+        {
+            InitializeComponent();
+            UserService = new UserService();
+            LoggedUser = user;
+            DataContext = LoggedUser;
+
+        }
+        public void loadUser(User user) 
+        {
+            DataContext = user;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
