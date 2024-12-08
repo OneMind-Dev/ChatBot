cd2b73589ff9ec754eb49281393f418e706451c4
Nguyễn Thương Huyền <phanvu211194@gmail.com>
2024-12-08 19:08:24 +0700

    Revert "update AI avatar"
    This reverts commit ac593374e50de32d8c041dee6517da01324cf99f.

--- ChatBot/WPF/ChatWindow.xaml.cs
+++ ChatBot/WPF/ChatWindow.xaml.cs
@@ -96,7 +96,7 @@ namespace WPF
                 Text = aiResponse,
                 BackgroundColor = "#3C3F41",
                 HorizontalAlignment = HorizontalAlignment.Left,
-                Avatar = "C:\\Users\\thuon\\OneDrive\\Documents\\GitHub\\ChatBot\\ChatBot\\WPF\\Icons\\images.png",
+                Avatar = "path_to_ai_avatar.png", // Replace with actual image path
                 AvatarVisibility = Visibility.Visible
             });
         }
